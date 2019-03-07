3f8baf0772321a7547de1a4d83419e386192ad1e
Abhishek Anand <abhishek197770@gmail.com>
2019-03-07 13:02:06 +0530

    Added Compiler Versions

--- decode_api/models.py
+++ decode_api/models.py
@@ -161,7 +161,9 @@ class TemplateCode(models.Model):
 
     """Stores the Code Template for the Language ID"""
     language_id = models.CharField(max_length=2)
+    name = models.CharField(max_length=256, default = "")
     template_code = models.TextField(null = True, blank=True)
     compiler = models.CharField(max_length=256, default="")
     time_limit = models.CharField(max_length=18, default="10 Sec")
     memory_limit = models.CharField(max_length=20, default="150Mb")
+
